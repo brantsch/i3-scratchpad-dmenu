a60a7618442966fadcb4ff6fcec9d8d144510ba1
Peter Brantsch <peter@brantsch.name>
2017-03-25 12:58:49 +0100

    basically working

---
+++ i3-scratchpad-dmenu.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import i3ipc
+import subprocess
+
+i3 = i3ipc.Connection()
+
+def get_scratchpad_windows():
+    scratchpad_containers = i3.get_tree().scratchpad().descendents()
+    return filter(lambda c: c.type == 'con', scratchpad_containers)
+
+def dmenu_choose(options):
+    """ Show a dmenu to choose a string item from a list of *options*. """
+    dmenu_process = subprocess.Popen(["dmenu"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdoutdata, _ = dmenu_process.communicate("\n".join(options).encode())
+    return stdoutdata.decode('utf-8')
+
+def main():
+    scratchpad_windows = get_scratchpad_windows()
+    window_titles = [w.name for w in scratchpad_windows]
+    window_to_restore = dmenu_choose(window_titles).strip()
+    i3.command('[title="{}"] scratchpad show'.format(window_to_restore))
+
+if __name__ == '__main__':
+    main()
